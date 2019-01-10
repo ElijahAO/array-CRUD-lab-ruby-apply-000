38a364bbdee4a2a7937acc0a85f11c6df4ca2a02
Elijah Adebiyi Oyeniyi <biyibama@gmail.com>
2019-01-10 12:01:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,11 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  ["hi", "hello", "who", "ok"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
-end
-
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
+def add_element_to_end_of_array
+ ["hi!", "hello!", "who!", "ok!"]
 end
