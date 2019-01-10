a9a4dd060501615ee06ff6c0f2738443bda66332
Elijah Adebiyi Oyeniyi <biyibama@gmail.com>
2019-01-10 13:05:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,8 +3,10 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["hi", "hello", "thanks", "please",]
+  ["hi", "hello", "thanks", "please"]
   
 end
 
-def c
+def add_element_to_end_array
+  ["wow", "i", "am", "really", "learning", <<"!"]
+end
